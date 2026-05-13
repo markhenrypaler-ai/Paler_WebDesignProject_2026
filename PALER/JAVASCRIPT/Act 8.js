3aa76b798295988f7783b8a32d9da36a60f2d12f
markhenrypaler-ai <markhenry.paler@gmail.com>
2026-05-13 16:40:05 +0800

    Delete PALER directory

--- PALER/JAVASCRIPT/Act 8.js
+++
@@ -1,9 +0,0 @@
-function getInnerText() {
-alert(document.getElementById("example").innerText)
-}
-function getInnerHTML() {
-alert(document.getElementById("example").innerHTML)
-}
-function getTextContent() {
-alert(document.getElementById("example").textContent)
-}
